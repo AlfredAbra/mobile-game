29f1fddbf5c04a8f113b2eb6e61a81493cf7bab5
AndrewSullivan <andrewsullivan121@outlook.com>
2021-10-23 13:40:51 +0100

    Created base maze level with materials

--- Tilting Maze Mobile Game/Assets/Scripts/BallMovement.cs
+++ Tilting Maze Mobile Game/Assets/Scripts/BallMovement.cs
@@ -6,7 +6,7 @@ public class BallMovement : MonoBehaviour
 {
     Rigidbody rb;
 
-    float ballSpeed = 5f;
+    float ballSpeed = 10f;
 
     bool notTilting = true;
 
@@ -20,7 +20,7 @@ public class BallMovement : MonoBehaviour
     void Update()
     {
         // The acceleration.y variable is used within the z-axis part of the Vector3 variable so it matches the phone's tilt correctly (so if the phone is tilted forward it moves on te z-axis in the game scene).
-        Vector3 ballTilt = new Vector3 (Input.acceleration.x,0f,Input.acceleration.y);
+        Vector3 ballTilt = new Vector3 (Input.acceleration.x,0f, Input.acceleration.y);
 
         // This adds the velocity to the ball game object and also uses the ballSpeed value as well to move at a certain speed.
         rb.velocity = ballTilt * ballSpeed;
